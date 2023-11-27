a850fe86edd8a307ca1791f7ca644dfb67c4406b
Laith007 <laithalmajaly007@gmail.com>
2023-11-28 00:35:48 +0300

    added a purchase table

--- Server/server.js
+++ Server/server.js
@@ -16,8 +16,8 @@ const commentsRoute = require("./routes/commentsRouter");
 //* Cart Router
 const cartRoute = require("./routes/cartRouter");
 
-//* Payments Router
-const paymentsRoute = require("./routes/paymentsRouter");
+//* Purchases Router
+const purchasesRoute = require("./routes/purchasesRouter");
 
 app.use(cors());
 app.use(express.json());
@@ -26,7 +26,7 @@ app.use(usersRoute);
 app.use(coursesRoute);
 app.use(commentsRoute);
 app.use(cartRoute);
-app.use(paymentsRoute);
+app.use(purchasesRoute);
 
 // Call the initialization function to create tables
 models.initializeModels();

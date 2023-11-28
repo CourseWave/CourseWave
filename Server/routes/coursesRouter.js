71c66e1e88caf9e1701039e51f4a56a2d44fb8a2
Laith007 <laithalmajaly007@gmail.com>
2023-11-29 00:58:13 +0300

    modified the courses table to take objectives and requierments

--- Server/routes/coursesRouter.js
+++ Server/routes/coursesRouter.js
@@ -4,11 +4,6 @@ const coursesController = require("../controllers/coursesController");
 const verify = require("../middlewares/verify");
 
 router.post("/addCourse", verify.authorize, coursesController.addCourse);
-router.post("/addCourseObject/:course_id", coursesController.addCourseObject);
-router.post(
-  "/addCourseRequirement/:course_id",
-  coursesController.addCourseRequirement
-);
 router.post("/addCourseSection/:course_id", coursesController.addCourseSection);
 router.post(
   "/addCourseVideos/:course_section_id",
@@ -16,14 +11,7 @@ router.post(
 );
 
 router.put("/updateCourse/:course_id", coursesController.updateCourse);
-router.put(
-  "/updateCourseObject/:object_id",
-  coursesController.updateCourseObject
-);
-router.put(
-  "/updateCourseRequirement/:requirement_id",
-  coursesController.updateCourseRequirement
-);
+
 router.put(
   "/updateCourseSection/:course_section_id",
   coursesController.updateCourseSection
@@ -31,14 +19,6 @@ router.put(
 router.put("/updateCourseVideo/:video_id", coursesController.updateCourseVideo);
 
 router.put("/deleteCourse/:course_id", coursesController.deleteCourse);
-router.put(
-  "/deleteCourseObject/:object_id",
-  coursesController.deleteCourseObject
-);
-router.put(
-  "/deleteCourseRequirement/:requirement_id",
-  coursesController.deleteCourseRequirement
-);
 router.put(
   "/deleteCourseSection/:course_section_id",
   coursesController.deleteCourseSection
@@ -50,14 +30,6 @@ router.put(
 
 router.get("/getCourses", coursesController.getCourses);
 router.get("/getCourse/:course_id", coursesController.getCourse);
-router.get(
-  "/getCourseObjectDetails/:course_id",
-  coursesController.getCourseObjectDetails
-);
-router.get(
-  "/getCourseRequirementDetails/:course_id",
-  coursesController.getCourseRequirementDetails
-);
 router.get(
   "/getCourseSections/:course_id",
   coursesController.getCourseSections
@@ -79,3 +51,36 @@ router.get(
 );
 
 module.exports = router;
+
+// router.post("/addCourseObject/:course_id", coursesController.addCourseObject);
+// router.post(
+//   "/addCourseRequirement/:course_id",
+//   coursesController.addCourseRequirement
+// );
+
+// router.put(
+//   "/updateCourseObject/:object_id",
+//   coursesController.updateCourseObject
+// );
+// router.put(
+//   "/updateCourseRequirement/:requirement_id",
+//   coursesController.updateCourseRequirement
+// );
+
+// router.put(
+//   "/deleteCourseObject/:object_id",
+//   coursesController.deleteCourseObject
+// );
+// router.put(
+//   "/deleteCourseRequirement/:requirement_id",
+//   coursesController.deleteCourseRequirement
+// );
+
+// router.get(
+//   "/getCourseObjectDetails/:course_id",
+//   coursesController.getCourseObjectDetails
+// );
+// router.get(
+//   "/getCourseRequirementDetails/:course_id",
+//   coursesController.getCourseRequirementDetails
+// );
